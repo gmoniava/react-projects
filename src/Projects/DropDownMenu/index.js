5ba5f67db5a42e9a9e4d3b21bec0eb60b5b29fd5
giorgim <moniava.g@gmail.com>
2021-11-28 22:24:34 +0400

    make menu responsive

--- src/Projects/DropDownMenu/index.js
+++ src/Projects/DropDownMenu/index.js
@@ -1,11 +1,33 @@
 import React from "react";
 import "./index.css";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+import { useMediaQuery } from "react-responsive";
 
-export default function App() {
+// Responsive dropdown menu
+
+export default function DropDownMenu() {
+  const isMobile = useMediaQuery({ query: "(max-width: 624px)" });
+  let [isMenuOpen, setIsMenuOpen] = React.useState(false);
   return (
     <div className="dropdownmenu">
-      <nav role="navigation">
-        <ul>
+      <nav
+        // Makes sense to have mobile menu open only on mobile screen and when menu is in open state
+        className={isMenuOpen && isMobile ? "open" : ""}
+        role="navigation"
+        style={{
+          backgroundColor: "darkorange",
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <ul
+          style={{
+            flex: 1,
+            minWidth: 0,
+            // Menu items are hiddden only when we are on mobile and the menu is in closed state
+            visibility: isMobile && !isMenuOpen ? "hidden" : "",
+          }}
+        >
           <li>
             <a href="/#">One</a>
           </li>
@@ -43,6 +65,22 @@ export default function App() {
             <a href="/#">Three</a>
           </li>
         </ul>
+        <div style={{ cursor: "pointer", marginRight: 5 }}>
+          {isMobile &&
+            (isMenuOpen ? (
+              <AiOutlineClose
+                onClick={() => {
+                  setIsMenuOpen(false);
+                }}
+              />
+            ) : (
+              <AiOutlineMenu
+                onClick={() => {
+                  setIsMenuOpen(true);
+                }}
+              />
+            ))}
+        </div>
       </nav>
     </div>
   );
