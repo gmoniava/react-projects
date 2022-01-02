57cff7ef731b47a8202fa4f5ed92139bbef16da0
giorgim <moniava.g@gmail.com>
2022-01-02 22:49:14 +0400

    some refactor inside ripple btn

--- src/Projects/RippleButton/index.js
+++ src/Projects/RippleButton/index.js
@@ -7,31 +7,31 @@ import "./index.css";
 //
 
 export default function RippleButton() {
-  let [coords, setCoords] = React.useState();
+  let [rippleCoords, setRippleCoords] = React.useState();
   let btnRef = React.useRef();
+  let handleClick = (e) => {
+    // We must determine where was clicked on the button (in local coordinates)
+    const relativeToParentX =
+      e.clientX - btnRef.current.getBoundingClientRect().left;
+    const relativeToParentY =
+      e.clientY - btnRef.current.getBoundingClientRect().top;
+
+    setRippleCoords({
+      x: relativeToParentX,
+      y: relativeToParentY,
+      // Needs a new key each time (mainly for animation).
+      key: ((rippleCoords?.key || 0) + 1) % 100,
+    });
+  };
+
   return (
     <div className="ripple-btn" style={{ marginLeft: 200, paddingTop: 50 }}>
-      <button
-        ref={btnRef}
-        className="ripple"
-        onClick={(e) => {
-          const relativeToParentX =
-            e.clientX - btnRef.current.getBoundingClientRect().left;
-          const relativeToParentY =
-            e.clientY - btnRef.current.getBoundingClientRect().top;
-          setCoords({
-            x: relativeToParentX,
-            y: relativeToParentY,
-            // dummy used for generating new key on each click, so that animation is re applied.
-            dummy: ((coords?.dummy || 0) + 1) % 100,
-          });
-        }}
-      >
-        {coords && (
+      <button ref={btnRef} className="ripple" onClick={handleClick}>
+        {rippleCoords && (
           <span
-            key={coords.dummy}
+            key={rippleCoords.key}
             className="circle"
-            style={{ top: coords.y, left: coords.x }}
+            style={{ top: rippleCoords.y, left: rippleCoords.x }}
           ></span>
         )}
         Ripple btn
