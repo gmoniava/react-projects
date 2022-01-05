34a0bea0c563b5917caf5f9f09ad348284c11b62
giorgim <moniava.g@gmail.com>
2022-01-05 23:10:35 +0400

    rewrite ripple btn using styled comps

--- src/Projects/RippleButton/index.js
+++ src/Projects/RippleButton/index.js
@@ -1,39 +1,67 @@
 import React from "react";
-import "./index.css";
+import styled, { keyframes } from "styled-components";
 
 ///
 //
 // Button with ripple effect
 //
 
+const fadeInAndScale = keyframes`
+  to {
+    transform: translate(-50%, -50%) scale(3);
+    opacity: 0;
+  }
+`;
+
+let ButtonStyled = styled.button`
+  background-color: royalblue;
+  color: #fff;
+  border: 1px royalblue solid;
+  font-size: 14px;
+  letter-spacing: 2px;
+  padding: 20px 30px;
+  overflow: hidden;
+  cursor: pointer;
+  position: relative;
+`;
+
+let FadeInCircleStyled = styled.span`
+  position: absolute;
+  background-color: white;
+  top: ${({ top }) => top}px;
+  left: ${({ left }) => left}px;
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  z-index: 99999;
+  transform: translate(-50%, -50%) scale(0);
+  animation: ${fadeInAndScale} 1.5s ease-out;
+`;
+
 export default function RippleButton() {
   let [rippleCoords, setRippleCoords] = React.useState();
   let btnRef = React.useRef();
   let handleClick = (e) => {
-    // We must determine where was clicked on the button (in local coordinates)
     const localX = e.clientX - btnRef.current.getBoundingClientRect().left;
     const localY = e.clientY - btnRef.current.getBoundingClientRect().top;
 
     setRippleCoords({
       x: localX,
       y: localY,
-      // Needs a new key each time (mainly for animation).
       key: ((rippleCoords?.key || 0) + 1) % 100,
     });
   };
 
   return (
-    <div className="ripple-btn" style={{ marginLeft: 200, paddingTop: 50 }}>
-      <button ref={btnRef} className="ripple" onClick={handleClick}>
-        {rippleCoords && (
-          <span
-            key={rippleCoords.key}
-            className="circle"
-            style={{ top: rippleCoords.y, left: rippleCoords.x }}
-          ></span>
-        )}
-        Ripple btn
-      </button>
-    </div>
+    <ButtonStyled ref={btnRef} onClick={handleClick}>
+      {rippleCoords && (
+        <FadeInCircleStyled
+          key={rippleCoords.key}
+          top={rippleCoords.y}
+          left={rippleCoords.x}
+        />
+      )}
+      Ripple btn
+    </ButtonStyled>
   );
 }
