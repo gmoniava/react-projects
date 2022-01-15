c4ca37b70d211b570a37fec98c21d6535e43c4ad
gmoniava <moniava.g@gmail.com>
2022-01-15 23:16:36 +0400

    add new import

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
 import React from "react";
-import SearchBtn from "./Projects/SearchBtn"
-import Carousel from "./Projects/Carousel"
+import SearchBtn from "./Projects/SearchBtn";
+import Carousel from "./Projects/Carousel";
+import CnnNextBtn from "./Projects/CnnNextBtn";
 
 function App() {
   return (
     <div>
       {" "}
-      <Carousel />{" "}
+      <CnnNextBtn />{" "}
     </div>
   );
 }
