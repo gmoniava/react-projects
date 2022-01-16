68494a695634e656ccd375e0fb6c7e0c434a9d5f
gmoniava <moniava.g@gmail.com>
2022-01-16 15:02:26 +0400

    minor changes in ripple btn

--- src/Projects/RippleButton/index.js
+++ src/Projects/RippleButton/index.js
@@ -33,7 +33,6 @@ let FadeInCircleStyled = styled.span`
   width: 100px;
   height: 100px;
   border-radius: 50%;
-  z-index: 99999;
   transform: translate(-50%, -50%) scale(0);
   animation: ${fadeInAndScale} 1.5s ease-out;
 `;
@@ -41,10 +40,12 @@ let FadeInCircleStyled = styled.span`
 export default function RippleButton() {
   let [rippleCoords, setRippleCoords] = React.useState();
   let btnRef = React.useRef();
+
   let handleClick = (e) => {
     const localX = e.clientX - btnRef.current.getBoundingClientRect().left;
     const localY = e.clientY - btnRef.current.getBoundingClientRect().top;
 
+    // Generate data for the new effect.
     setRippleCoords({
       x: localX,
       y: localY,
@@ -54,6 +55,7 @@ export default function RippleButton() {
 
   return (
     <ButtonStyled ref={btnRef} onClick={handleClick}>
+      {/* The circle with fade effect */}
       {rippleCoords && (
         <FadeInCircleStyled
           key={rippleCoords.key}
