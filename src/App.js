c6befec28ab653b9fe94552a7d885900ba85c81d
gmoniava <moniava.g@gmail.com>
2022-01-16 15:02:47 +0400

    import ripple btn in app

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import React from "react";
 import SearchBtn from "./Projects/SearchBtn";
 import Carousel from "./Projects/Carousel";
 import CnnNextBtn from "./Projects/CnnNextBtn";
-
+import RippleButton from "./Projects/RippleButton";
 function App() {
   return (
     <div>
       {" "}
-      <CnnNextBtn />{" "}
+      <RippleButton />{" "}
     </div>
   );
 }
