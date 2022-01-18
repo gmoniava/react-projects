69cc3b7b47c4022c13d17c85bee34d67cc38aefa
gmoniava <moniava.g@gmail.com>
2022-01-19 00:55:23 +0400

    add comment

--- src/Projects/TreeList/index.js
+++ src/Projects/TreeList/index.js
@@ -105,6 +105,8 @@ export default function TreeList({
   style,
 }) {
   let [currentNode, setCurrentNode] = React.useState({
+    // We need a unique id for root node, because this is used as key, when drawing full path.
+    // This value is very unlikely to collide with user supplied ids of tree nodes.
     id: "treelist-root-id-0001",
     children: initialData,
   });
