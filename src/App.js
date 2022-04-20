bba33b5cd7f875b165e454d806455add9dc853fe
gmoniava <moniava.g@gmail.com>
2022-04-21 01:37:51 +0400

    delete some projects

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import "./App.css";
 import React from "react";
-import SearchBtn from "./Projects/SearchBtn";
 import Carousel from "./Projects/Carousel";
 import CnnNextBtn from "./Projects/CnnNextBtn";
-import RippleButton from "./Projects/RippleButton";
 import StackOverflowAnimation from "./Projects/StackOverflowAnimation";
 import Tree from "./Projects/Tree";
 import TreeList from "./Projects/TreeList";
 import Snake from "./Projects/Snake";
 import Minesweeper from "./Projects/Minesweeper";
-import SideMenu from "./Projects/SideMenu";
 function App() {
   return (
     <div>
-      {" "}
-      <SideMenu />
+      <Carousel />{" "}
     </div>
   );
 }
